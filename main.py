5bbe41b86e043e7b9070b1359f94600da4062e00
PotentialAviator15 <dontbegullible6@gmail.com>
2025-02-22 11:04:33 -0600

    not sure what I did but here u go

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-
 def calculate():
-    name = input("Player's name: ") #get the player name
-    at_bats = int(input("Official number of at bats: ")) #get the at_bats 
-    hits = int(input("Number of hits: ")) #get the amount of hits 
+    name = input("Player's name: ") # get the player name
+    at_bats = int(input("Official number of at bats: ")) # get the at_bats 
+    hits = int(input("Number of hits: ")) # get the amount of hits 
     
     if at_bats > 0:
         batting_average = hits / at_bats
     else:
         batting_average = 0.0 
     
-    print(f"\n{name}'s batting average is {batting_average:.3f}") #print the calculation to the terminal
-
-
+    print(f"\n{name}'s batting average is {batting_average:.3f}") # print the calculation to the terminal
 
 def menu():
     print("=" * 63)
@@ -21,14 +18,24 @@ def menu():
     print("1 - Calculate batting average")
     print("2 - Exit the program")
     print("=" * 63)
-    menuOption = int(input("Menu option:  "))
+
+def main():
+    menu()
+    menuOption = input("Menu option:  ")
+    # if its a letter input this clause will trigger down below, just further error handling
+    if not menuOption.isdigit():
+        print("Menu options come in numbers, not letters. This isn't English class.")
+        main()
+        return  # exit function to prevent further execution, and to improve efficiency
+
+    menuOption = int(menuOption)  # from here down it will work correctly for checking menu options
+
     if menuOption == 1:
         calculate()
     elif menuOption == 2:
         print("Bye!")
     else:
         print("Invalid option. Please choose a valid menu option.")
-        menu()
+        main() # repeats the menu and the function, which allows for 
 
-
-menu()
+main()
