d77f5abee4aed60201abf050c52b78335ed5a687
PotentialAviator15 <dontbegullible6@gmail.com>
2025-03-02 00:19:12 -0600

    3/2/25 submission

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-def calculate():
-    name = input("Player's name: ") # get the player name
-    at_bats = int(input("Official number of at bats: ")) # get the at_bats 
-    hits = int(input("Number of hits: ")) # get the amount of hits 
-    
-    if at_bats > 0:
-        batting_average = hits / at_bats
-    else:
-        batting_average = 0.0 
-    
-    print(f"\n{name}'s batting average is {batting_average:.3f}") # print the calculation to the terminal
+def calculate(name, hits, at_bats):
+
+    batting_average = hits / at_bats if at_bats > 0 else 0.0 ## ze math
+
+    # Display the result
+    print(f"\n{name}'s batting average is {batting_average:.3f}")
+
+ # print the calculation to the terminal
 
 def menu():
     print("=" * 63)
@@ -23,19 +20,29 @@ def main():
     menu()
     menuOption = input("Menu option:  ")
     # if its a letter input this clause will trigger down below, just further error handling
-    if not menuOption.isdigit():
-        print("Menu options come in numbers, not letters. This isn't English class.")
-        main()
-        return  # exit function to prevent further execution, and to improve efficiency
-
+    # if not menuOption.isdigit():
+    #     print("Menu options come in numbers, not letters. This isn't English class.")
+    #     main()
+    #     return  # exit function to prevent further execution, and to improve efficiency
     menuOption = int(menuOption)  # from here down it will work correctly for checking menu options
-
     if menuOption == 1:
-        calculate()
+        name = input("Player's name: ") 
+        at_bats = int(input("Official number of at-bats: "))  ## the at bats input
+        if at_bats < 0:
+            print("At-bats cannot be negative. Exiting program.")
+            return
+        hits = int(input("Number of hits: "))  ## the hits input 
+        if hits < 0:
+            print("Invalid number of hits. Exiting program.")
+            return
+        if hits > at_bats:
+            print("Hits cannot be greater than at-bats. Exiting program.")
+            return
+        calculate(name, hits, at_bats)
     elif menuOption == 2:
         print("Bye!")
     else:
         print("Invalid option. Please choose a valid menu option.")
-        main() # repeats the menu and the function, which allows for 
+        main() # function recursion
 
 main()
